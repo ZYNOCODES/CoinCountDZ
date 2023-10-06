1148ed83186d27aad8cf3c6d08c4f35ddebafada
moumen26 <moumenkhaldi26@gmail.com>
2023-10-06 12:00:53 +0100

    Wallet page

--- WebApp/frontend/src/components/CardGoal.jsx
+++ WebApp/frontend/src/components/CardGoal.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function CardAccount() {
+export default function CardGoal() {
   return (
-    <div className="CardAccount flex flex-col justify-between p-3">
+    <div className="CardSaving flex flex-col justify-between p-3">
       <span className="date">Oct 05</span>
       <div className="total-sales">
-        <span>Account balance</span>
+        <span>Goal</span>
         <h3>
           212,376<span>DA</span>
         </h3>
--- WebApp/frontend/src/pages/Wallet.jsx
+++ WebApp/frontend/src/pages/Wallet.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import NavBar from '../components/NavBar';
-import Calendar from '../components/Calendar';
+import React from "react";
+import NavBar from "../components/NavBar";
+import Calendar from "../components/Calendar";
+import CardFITTECH from "../components/CardFITTECH";
+import CardAccount from "../components/CardAccount";
+
+import { BsFillFileEarmarkArrowUpFill } from "react-icons/bs";
+import { BiSolidBank } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import CardSaving from "../components/CardSaving";
+import CardSpending from "../components/CardSpending";
+import CardSpendingMoy from "../components/CardSpendingMoy";
+import CardGoal from "../components/CardGoal";
 
 export default function Wallet() {
-  return <div className="Wallet">
-    <NavBar />
-    <Calendar />
-  </div>;
+  return (
+    <div className="Wallet flex flex-col">
+      <div className="Wallet-header flex flex-col">
+        <NavBar />
+        <div className="Wallet-header-container flex mt-5 gap-8">
+          <div className="header-left flex flex-col gap-8">
+            <span>Wallet</span>
+            <CardFITTECH />
+            <div className="rapport flex mt-8 gap-8">
+              <CardAccount />
+              <div className="rapport-item flex items-center gap-2">
+                <BsFillFileEarmarkArrowUpFill className="icon" />
+                <span>Rapport</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="header-right grid">
+            <CardSpending />
+            <CardSpendingMoy />
+            <CardSaving />
+            <CardGoal />
+            <CardSaving />
+            <CardSaving />
+          </div>
+        </div>
+      </div>
+      <div className="Wallet-Bank">
+        <div className="Bank-title">
+          <BiSolidBank />
+          <h2>Bank</h2>
+          <div className="add-class">
+            <AiOutlinePlus />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
