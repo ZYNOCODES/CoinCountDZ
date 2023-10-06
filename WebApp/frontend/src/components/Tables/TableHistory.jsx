b456cacedb552ea97c55144ae2f59911b3ce93d5
ZYNOCODES <zineeddine.boumrar@gmail.com>
2023-10-06 19:53:52 +0100

    transaction commit

--- WebApp/frontend/src/components/Tables/TableHistory.jsx
+++ WebApp/frontend/src/components/Tables/TableHistory.jsx
@@ -1,70 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TableHistoryReadOnlyRow from "./TableHistoryReadOnlyRow";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function TableHistory() {
-
-    const HistoryDB = [
-      {
-        id: 1,
-        Name: "FACEBK XG2..",
-        Date: "April 28,2023 at 11:00",
-        From: "Acount-d",
-        To: "FACEBK",
-        Amount: "40",
-      },
-      {
-        id: 2,
-        Name: "FACEBK XG2..",
-        Date: "April 28,2023 at 11:00",
-        From: "Acount-id",
-        To: "FACEBK",
-        Amount: "40",
-      },
-      {
-        id: 3,
-        Name: "FACEBK XG2..",
-        Date: "April 28,2023 at 11:00",
-        From: "Acount-id",
-        To: "FACEBK",
-        Amount: "40",
-      },
-      {
-        id: 4,
-        Name: "FACEBK XG2..",
-        Date: "April 28,2023 at 11:00",
-        From: "Acount-id",
-        To: "FACEBK",
-        Amount: "40",
-      },
-      {
-        id: 5,
-        Name: "FACEBK XG2..",
-        Date: "April 28,2023 at 11:00",
-        From: "Acount-id",
-        To: "FACEBK",
-        Amount: "40",
-      },
-    ];
-
+  const { user } = useAuthContext();
+  const [TransactionData, setTransactionData] = useState('')
+  // Fetch Transaction Data
+  useEffect(() => {
+    const fetchTransactionData = async () => {
+      if (user?.token !== undefined) {
+        await fetch(`http://localhost:8000/Transaction`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }).then((response) => {
+          if (response.ok) {
+            response
+              .json()
+              .then((data) => {
+                setTransactionData(data);
+              })
+              .catch((error) => {
+                console.error("Error fetching Transaction data:", error);
+              });
+          } else {
+            console.error("Error resieving Transaction date", response.error);
+          }
+        });
+      } 
+    };
+    fetchTransactionData();
+  }, [TransactionData, user?.token]);
+  
+  
   return (
     <div className="TableHistory flex flex-col gap-5">
       <th className="flex item-center gap-1">
         <td>ID</td>
         <td>Date & Time</td>
+        <td>Type</td>
         <td>From</td>
         <td>To</td>
         <td>Amount</td>
       </th>
       <div className="Table-history flex flex-col gap-4">
-        {HistoryDB.map((history) => {
+        {TransactionData && TransactionData?.map((history) => {
           return (
             <TableHistoryReadOnlyRow
               key={history.id}
               Name={history.Name}
-              Date={history.Date}
-              From={history.From}
-              To={history.To}
+              CreatedDate={history.createdAt}
+              From={history.Sender}
+              To={history.Reciver}
               Amount={history.Amount}
+              type={history.Type}
             />
           );
         })}
--- WebApp/frontend/src/pages/Home.jsx
+++ WebApp/frontend/src/pages/Home.jsx
@@ -40,6 +40,7 @@ export default function Home() {
     };
     fetchWalletData();
   }, [WalletData, user?.token]);
+  
   return (
     <div className="Home flex">
       <NavBar />
